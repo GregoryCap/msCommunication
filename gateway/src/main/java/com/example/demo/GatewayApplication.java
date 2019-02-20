c8393cdf52b12f6863784287a3b2025073fd2fd8
LIBERT <gregory.libert@capgemini.com>
2019-02-20 16:42:17 +0100

    First commit

---
+++ gateway/src/main/java/com/example/demo/GatewayApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+import com.example.demo.filter.pre.SimpleFilter;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+	
+	public SimpleFilter simpleFilter() {
+		return new SimpleFilter();
+	}
+
+}
